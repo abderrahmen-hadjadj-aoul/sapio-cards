7e78918a65378e07cf8b9187517873a9f031fc77
Abderrahmen <abder78@gmail.com>
2021-03-25 14:26:43 +0100

    Updated email from field

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -22,6 +22,7 @@ class RegistrationController extends AbstractController
     public function __construct(EmailVerifier $emailVerifier)
     {
         $this->emailVerifier = $emailVerifier;
+        $this->from = new Address("contact@hadjadjaoul.com", "Sapio-Cards - No respond");
     }
 
     /**
@@ -52,7 +53,7 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('mailer@your-domain.com', 'Acme Mail Bot'))
+                    ->from($this->from)
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
@@ -77,7 +78,7 @@ class RegistrationController extends AbstractController
         }
         $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
             (new TemplatedEmail())
-                ->from(new Address('mailer@your-domain.com', 'Acme Mail Bot'))
+                ->from($this->from)
                 ->to($user->getEmail())
                 ->subject('Please Confirm your Email')
                 ->htmlTemplate('registration/confirmation_email.html.twig')
